e11faf39aa16daa4b17ed59a67cb3e014f918af1
vtsuellen <vtsuellen@hotmail.com>
2023-05-26 13:02:09 -0300

    requirement 2

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -7,7 +7,13 @@ export const searchCities = async (term) => {
   if (!data.length) window.alert('Nenhuma cidade encontrada');
   return data;
 };
-
-export const getWeatherByCity = (/* cityURL */) => {
-  //   seu código aqui
+export const getWeatherByCity = async (cityURL) => {
+  const response = await fetch(`http://api.weatherapi.com/v1/current.json?lang=pt&key=${token}&q=${cityURL}`);
+  const data = await response.json();
+  const obj = {
+    temp: data.current.temp_c,
+    condition: data.current.condition.text,
+    icon: data.current.condition.icon,
+  };
+  return obj;
 };
